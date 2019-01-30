15b482e6cdc7cd59cca14b06ab72abca2addfc90
Kamal (Riwogo) <pmer93@gmail.com>
2019-01-30 23:59:35 +0330

    add datatypes example

---
+++ datatypes_example.hs
@@ -0,0 +1,29 @@
+data Price = Price Integer 
+             deriving (Eq, Show)
+
+data Size = Size Integer
+            deriving (Eq, Show)
+
+data Manufacturer = Mini | Mazda | Tata 
+                    deriving (Eq, Show)
+
+data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited 
+               deriving (Eq, Show)
+
+data Vehicle = Car Manufacturer Price | Plane Airline Size
+               deriving (Eq, Show)
+
+isCar :: Vehicle -> Bool
+isCar (Car _ _) = True
+isCar _ = False 
+
+isPlane :: Vehicle -> Bool
+isPlane (Plane _ _) = True
+isPlane _ = False
+
+areCars :: [Vehicle] -> [Bool]
+areCars = map isCar
+
+getManu :: Vehicle -> Maybe Manufacturer
+getManu (Car manufacture _) = Just manufacture
+getManu _ = Nothing
