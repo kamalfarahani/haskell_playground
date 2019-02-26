9cbf77ed76752f54b568dac8692d822809bc06ec
Kamal (Riwogo) <pmer93@gmail.com>
2019-02-27 00:17:53 +0330

    add capitalizeParagraph

--- str_ex.hs
+++ str_ex.hs
@@ -1,6 +1,7 @@
 module StrEx where
 
 import Data.Char
+import Data.List
 
 isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
 isSubsequenceOf xs1 xs2 = all (flip elem $ xs2) xs1
@@ -10,7 +11,19 @@ capitalizeWords text = zip words capWords
     where words = splitOn text ' '
           capWords = map capitalizeWord words
 
+capitalizeParagraph :: String -> String
+capitalizeParagraph "" = ""
+capitalizeParagraph text = intercalate "." capParagraphs
+    where paragraphs = splitOn text '.'
+          capParagraphs = map (capFirstWord) paragraphs
+          capFirstWord paragraph = firstSpaces ++ (capitalizeWord firstWord) ++ rest
+              where firstWord = takeWhile (/= ' ') $ drop (length firstSpaces) paragraph
+                    firstSpaces = takeWhile (==' ') paragraph
+                    rest = drop (length firstWord + length firstSpaces) paragraph
+          
+
 capitalizeWord :: String -> String
+capitalizeWord "" = ""
 capitalizeWord word = toUpper (head word) : tail word
 
 splitOn :: String -> Char -> [String]
