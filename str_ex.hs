a2b85fbbb809c1693aeecfaa76fa620b8a1f0cdf
Kamal (Riwogo) <pmer93@gmail.com>
2019-02-26 23:21:12 +0330

    add string exercises

---
+++ str_ex.hs
@@ -0,0 +1,20 @@
+module StrEx where
+
+import Data.Char
+
+isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
+isSubsequenceOf xs1 xs2 = all (flip elem $ xs2) xs1
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords text = zip words capWords
+    where words = splitOn text ' '
+          capWords = map cap words
+          cap = map toUpper
+
+splitOn :: String -> Char -> [String]
+splitOn "" _ = [""]
+splitOn (c:cs) char =
+    if c == char 
+        then "":rest
+        else (c:head rest):(tail rest)
+    where rest = splitOn cs char
