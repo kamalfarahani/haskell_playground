c0b3203169c61eab3981bbab394a7a4118377157
Kamal (Riwogo) <pmer93@gmail.com>
2019-02-26 23:33:07 +0330

    fix capitalizeWords

--- str_ex.hs
+++ str_ex.hs
@@ -8,13 +8,15 @@ isSubsequenceOf xs1 xs2 = all (flip elem $ xs2) xs1
 capitalizeWords :: String -> [(String, String)]
 capitalizeWords text = zip words capWords
     where words = splitOn text ' '
-          capWords = map cap words
-          cap = map toUpper
+          capWords = map capitalizeWord words
+
+capitalizeWord :: String -> String
+capitalizeWord word = toUpper (head word) : tail word
 
 splitOn :: String -> Char -> [String]
 splitOn "" _ = [""]
-splitOn (c:cs) char =
+splitOn (c : cs) char =
     if c == char 
-        then "":rest
-        else (c:head rest):(tail rest)
+        then "" : rest
+        else (c : head rest) : (tail rest)
     where rest = splitOn cs char
