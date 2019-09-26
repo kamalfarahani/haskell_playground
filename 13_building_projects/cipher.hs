8a6ca159dd4cae3c9b03e5950aff565c2886c844
Kamal (Riwogo) <pmer93@gmail.com>
2019-09-26 18:02:58 +0330

    add cipher exercise

---
+++ 13_building_projects/cipher.hs
@@ -0,0 +1,57 @@
+module Cipher where
+
+import Data.Char
+
+
+caesarEncryptInput :: IO String
+caesarEncryptInput = do
+    putStr "Enter the text: "
+    text <- getLine
+    putStr "Enter the key: "
+    keyStr <- getLine
+    let key = read keyStr
+    return $ caesarEncrypt text key
+
+caesarEncrypt :: String -> Int -> String
+caesarEncrypt str shift = map shiftChar lowerStr
+    where lowerStr = map toLower str 
+          shiftChar ch = chr $ shifted + ord 'a' 
+              where shifted = ((ord ch) - (ord 'a') + shift) `mod` 26
+
+caesarDecrypt :: String -> Int -> String
+caesarDecrypt str shift = caesarEncrypt str (-shift)
+
+vigenereEncryptInput :: IO String
+vigenereEncryptInput = do
+    putStr "Enter the text: "
+    text <- getLine
+    putStr "Enter the keyword: "
+    keyword <- getLine
+    return $ vigenereEncrypt text keyword
+
+vigenereEncrypt :: String -> String -> String
+vigenereEncrypt "" _ = ""
+vigenereEncrypt str keyword = go str (repeatKeyword str keyword) where
+    go "" _ = ""
+    go str' repeatedKeyword =
+        if head str' == ' ' 
+            then " " ++ go (tail str') (tail repeatedKeyword)
+            else  [shiftLetter (head str') shift] ++ go (tail str') (tail repeatedKeyword)
+            where shift = (ord $ toLower $ head repeatedKeyword) - ord 'a'
+
+shiftLetter :: Char -> Int -> Char
+shiftLetter ch shift = 
+    chr $ (((ord lowerChar) - aASCICode + shift) `mod` alphabetNumber) + aASCICode
+    where lowerChar = toLower ch
+          alphabetNumber = 26
+          aASCICode = ord 'a'
+          
+
+repeatKeyword :: String -> String -> String
+repeatKeyword str keyword = go str keyword where
+                go "" _ = ""
+                go str' "" = go str' keyword
+                go str' keyword' = 
+                    if head str' == ' ' 
+                        then " " ++ go (tail str') keyword' 
+                        else [head keyword'] ++ go (tail str') (tail keyword')
