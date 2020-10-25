e4272837df3c5b0b87042c353f924e0a92929c5a
Kamal (Riwogo) <pmer93@gmail.com>
2020-10-25 16:21:04 +0330

    add type signatures

--- 17_applicative/lookup.hs
+++ 17_applicative/lookup.hs
@@ -3,20 +3,24 @@ module Lookup where
 import Control.Applicative
 
 
+f :: Integer -> Maybe String
 f x = lookup x [
                   (3, "Hello"),
                   (4, "Julie"),
                   (5, "kbai")
                ]
 
+g :: Integer -> Maybe String
 g y = lookup y [
                   (7, "sup?"),
                   (8, "chris"),
                   (9, "aloha")
                ]
 
+h :: Integer -> Maybe Integer
 h z = lookup z [(2, 3), (5, 6), (7, 8)]
 
+m :: Integer -> Maybe Integer
 m x = lookup x [(4, 10), (8, 13), (1, 9001)]
 
 main = do
