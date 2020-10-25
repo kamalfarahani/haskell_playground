dbe24644581a3196c6171d2bfb4b5e5e38a7a1ae
Kamal (Riwogo) <pmer93@gmail.com>
2020-10-25 21:40:42 +0330

    add lookups exercise

---
+++ 17_applicative/lookupsExercise3.hs
@@ -0,0 +1,14 @@
+module LookupsExercise3 where
+
+
+xs = [1, 2, 3]
+ys = [4, 5, 6]
+
+x :: Maybe Integer
+x = lookup 3 $ zip xs ys
+
+y :: Maybe Integer
+y = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = sum <$> ( (,) <$> x <*> y )
